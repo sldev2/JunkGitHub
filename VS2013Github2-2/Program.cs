506f352f17a94e8508282b8c15081c43b2cd0cd9
sldev2 <slarres3@gmail.com>
2014-02-16 22:42:13 -0500

    First Commit

---
+++ VS2013Github2-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS2013Github2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
