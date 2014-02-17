6c84f33df9be5eef6b78e03ad804220d00bbf85c
sldev2 <slarres3@gmail.com>
2014-02-17 15:14:15 -0500

    added yes!

--- VS2013Github2-2/Program.cs
+++ VS2013Github2-2/Program.cs
@@ -11,7 +11,7 @@ namespace VS2013Github2_2
         static void Main(string[] args)
         {
             //this comment is in Branch 1
-            //this comment is in Branch 1.2 hey ho! oh, no!
+            //this comment is in Branch 1.2 hey ho! oh, no! yes, yes!
         }
     }
 }
